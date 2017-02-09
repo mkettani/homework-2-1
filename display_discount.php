abfd74ee2948370e59fe0f7f26e2e6d65df15e23
mkettani <mehdikettani89@gmail.com>
2017-02-08 19:31:06 -0500

    Update display_discount.php

--- display_discount.php
+++ display_discount.php
@@ -25,22 +25,22 @@
 </head>
 <body>
     <main>
-        <h1>This page is under construction</h1>
+        <h1>Product Discount Calculator</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $product_description_escaped; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price_formatted; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_percent_formatted; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_formatted; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_price_formatted; ?></span><br>
     </main>
 </body>
 </html>
